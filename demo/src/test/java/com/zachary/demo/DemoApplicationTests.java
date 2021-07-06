c42a38341fa0d5618516c041a3b2d9bc1ff3ccd8
zachary zhuo <courage0322@gmail.com>
2021-07-06 10:10:12 +0800

    Q1

---
+++ demo/src/test/java/com/zachary/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachary.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
